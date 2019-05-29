f7dcb06ac7dfb6ee0d6049b342695f9ff0a74dc8
bhadurianirban <bhaduri.anirban@gmail.com>
2019-05-29 15:24:44 +0530

    Trying network parameters with JGraphT

--- src/main/java/org/dgrf/networkcore/Driver.java
+++ src/main/java/org/dgrf/networkcore/Driver.java
@@ -16,7 +16,7 @@ import org.jgrapht.graph.DefaultEdge;
 public class Driver {
 
     public static void main(String args[]) {
-        String adjListFile = "/home/bhaduri/MEGA/DGRFFractal/testdata/network/Upload/Network.csv";
+        String adjListFile = "/home/dgrfi/MEGA/DGRFFractal/testdata/network/Upload/Network.csv";
         BuildGraph bg = new BuildGraph(adjListFile);
         Double acc = new AverageClusteringCoefficient(adjListFile).getAverageClustertingCoeffient();
         //Double lcc = clusteringCoefficient.getVertexScore("5");
