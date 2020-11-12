505dbbe1f4f196609d715dd4362f6a9af72c33ee
bhaduri <bhaduri.anirban@gmail.com>
2020-11-12 16:45:35 +0530

    Hedwig

--- src/main/java/org/patronus/networkcore/Driver.java
+++ src/main/java/org/patronus/networkcore/Driver.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.dgrf.networkcore;
+package org.patronus.networkcore;
 
 import java.math.BigDecimal;
 import java.util.AbstractSet;
